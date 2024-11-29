acbe932f38ae4de7630bf49de1d65c59812487a9
pi4erd <piuuttri@gmail.com>
2024-11-29 16:47:04 +0100

    install: Changed install script a little

--- install-linux.sh
+++ install-linux.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 CURRENT_VERSION=0.1.2
+INSTALL_DIR=/usr/local/bin
 
 ./build.sh # build
 
 tar -xf dist/treepy-$CURRENT_VERSION.tar.gz -C /tmp
 echo "Unpacked into /tmp/treepy-$CURRENT_VERSION"
-echo "Copying executable to /usr/local/bin/treepy. Requesting permissions..."
-sudo cp /tmp/treepy-$CURRENT_VERSION/src/tree.py /usr/local/bin/treepy
+echo "Installing executable to $INSTALL_DIR/treepy. Requesting permissions..."
+sudo install /tmp/treepy-$CURRENT_VERSION/src/tree.py $INSTALL_DIR/treepy
